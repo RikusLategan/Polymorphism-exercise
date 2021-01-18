ae333720b23135b89aa40a18c5e4b48e62c3b33f
RikusLategan <77432995+RikusLategan@users.noreply.github.com>
2021-01-18 15:38:18 +0200

    Add files via upload

--- shape.h
+++ shape.h
@@ -7,7 +7,7 @@ class Shape{
     virtual int volume() const {return 0;}
     
     virtual void printShapeName() const = 0;
-    virtual void print() const = 0;
+    virtual void print();
     //virtual void printDetails() const = 0;
 };
 
